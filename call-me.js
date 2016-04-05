0519b4236b201412d3f5d84fce8f52752a9ca144
Alsharapy2016 <afff471@gmail.com>
2016-04-05 22:58:26 +0300

    Create call-me.js

---
+++ call-me.js
@@ -0,0 +1,128 @@
+/*---اتصل بنا---*/
+.contact-button {
+    margin: 0 auto;
+    width: 100%;
+    height: 55px;
+    background-color: #2C2C2C;
+    border-radius: 0px 0px 0 0;
+    margin-top: 2px;
+    margin-bottom: -4px;
+background-image:url(http://www6.0zz0.com/2016/04/03/23/368261111.png);
+    }
+
+
+.contact-button a {
+    width: 55px;
+    padding: 18px;
+    color: #fff;
+    margin-right: 100px;
+    display: inline-block;
+    font-size: 16px;
+ background: url("http://www5.0zz0.com/2016/04/03/22/444277059.png") no-repeat scroll 50% center #dc311b;}
+
+}
+.contact-close:hover {
+    color: #0894D8;
+    background: #000;
+}
+.contact-sec {
+    border: 1px solid #0894D8;
+    position: fixed;
+    top: 50%;
+    margin-top: -190px;
+    left: 50%;
+    max-width: 300px;
+    width: 90%;
+    margin-left: -150px;
+    background-color: #FFF;
+    height: auto;
+    z-index: 99999;
+    display: none;
+    -webkit-transform: scale(0);
+    -moz-transform: scale(0);
+    transform: scale(0);
+    transition: all .5s ease;
+    -webkit-transition: all .2s ease;
+    -moz-transition: all .2s ease;
+    -o-transition: all .2s ease;
+
+}
+.contact-close {
+    position: absolute;
+    top: -13px;
+    background-color: #0894D8;
+    color: #FFF;
+    height: 25px;
+    width: 25px;
+    text-align: center;
+    border-radius: 25px;
+    line-height: 25px;
+    font-size: 10px;
+}
+.contact-sec .widget {
+    padding: 20px;
+}
+.contact-sec .contact-form-cross {
+    display: none;
+}
+#ContactForm1 h2 {
+    font-size: 18px;
+    text-align: center;
+    color: #0894D8;
+    border-bottom: 1px solid;
+    padding-bottom: 8px;
+}
+.contact-sec.contact-show {
+    -webkit-transform: scale(1);
+    -moz-transform: scale(1);
+    transform: scale(1);
+}
+contact-form-name, .contact-form-email, .contact-form-email-message, .contact-form-widget {
+    max-width: none;
+    margin-bottom: 10px;
+    padding: 5px;
+}
+.contact-form-name, .contact-form-email, .contact-form-email-message {
+    background-color: #f3f3f3;
+    width: 250px;
+    border: 0;
+}
+.contact-form-button-submit {
+    max-width: none;
+    width: 100%;
+    height: 35px;
+    border: 0;
+    background-image: none;
+    background-color: #0894D8;
+    cursor: pointer;
+    color: #fff;
+}
+.contact-form-name:focus, .contact-form-email:focus, .contact-form-email-message:focus {
+    border: 0;
+    box-shadow: none;
+}
+.contact-form-name:hover, .contact-form-email:hover, .contact-form-email-message:hover {
+    border: 0;
+}
+.contact-form-button-submit:hover {
+    background-color: #222;
+    background-image: none;
+    border: 0;
+}
+input#ContactForm1_contact-form-name, input#ContactForm1_contact-form-email{
+  background: #eee;
+  padding: 8px;
+}
+i.fa.fa-envelope {
+  margin-right: 14px;
+}
+.fa-envelope:before {
+  font-size: 30px;
+  display: block;
+  margin-bottom: 7px;
+}
+.fa-times:before {
+  content: "\f00d";
+  margin-right: 2px;
+}
+/*---نهاية كود اتصل بنا---*/
